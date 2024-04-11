780dc3da04bc6b2e366e8bd3d65e404b15998d21
Cjungwo <jwoo516@naver.com>
2024-04-11 19:55:50 +1000

    Initial Commit

---
+++ UIPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UIPractice
+//
+//  Created by JungWoo Choi on 11/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UIPractice/UIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIPracticeApp.swift
+//  UIPractice
+//
+//  Created by JungWoo Choi on 11/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct UIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
